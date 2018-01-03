aac1db9e0966d0bc6aa1898a8d335e44c7c8fbff
Marina Barbosa <marina.veiga.barbosa@gmail.com>
2018-01-03 17:18:37 +0000

    [1] Currency + Menu + Sensor + Maps

--- app/src/main/java/com/example/marin/ms_pb_17/IntroApp.java
+++ app/src/main/java/com/example/marin/ms_pb_17/IntroApp.java
@@ -3,35 +3,85 @@ package com.example.marin.ms_pb_17;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.ListView;
-
 import android.view.WindowManager;
 import android.view.Window;
-import android.widget.LinearLayout;
+import android.widget.FrameLayout;
 import android.view.View;
+import android.widget.RelativeLayout;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.content.Context;
 
-public class IntroApp extends Activity {
+public class IntroApp extends Activity implements SensorEventListener {
 
-    private ListView lvCurrency;
+    private SensorManager sensorManager;
+    private Sensor sensor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-        WindowManager.LayoutParams.FLAG_FULLSCREEN);
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_intro_app);
 
-        LinearLayout fullLayoutButton = (LinearLayout) findViewById(R.id.clickLayout);
+        FrameLayout fullLayoutButton = (FrameLayout) findViewById(R.id.layoutCurrency);
+        RelativeLayout fullLayoutButtonMap = (RelativeLayout) findViewById(R.id.layoutMap);
 
         fullLayoutButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                Intent intent = new Intent(IntroApp.this, Menu.class);
-                startActivity(intent);
-                finish();
+              Intent intent = new Intent(IntroApp.this, MainActivity.class);
+               startActivity(intent);
             }
         });
+        fullLayoutButtonMap.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(IntroApp.this, MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+        //declaring Sensor Manager and sensor type
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+    }
 
+    @Override
+    public void onAccuracyChanged(Sensor arg0, int arg1) {
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float x = event.values[0];
+        float y = event.values[1];
+        if (Math.abs(x) > Math.abs(y)) {
+            if (x < 0) {
+                Intent intent = new Intent(IntroApp.this, MapsActivity.class);
+                startActivity(intent);
+                //image.setImageResource(R.drawable.right);
+                //textView.setText("You tilt the device right");
+            }
+            if (x > 0) {
+                Intent intent = new Intent(IntroApp.this, MainActivity.class);
+                startActivity(intent);
+                //image.setImageResource(R.drawable.left);
+                //textView.setText("You tilt the device left");
+            }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //unregister Sensor listener
+        sensorManager.unregisterListener(this);
     }
 }
-
-
